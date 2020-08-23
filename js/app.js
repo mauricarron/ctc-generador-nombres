620dc7676be9b6984d73cff49b88cdede9553085
mdCarron <59117899+mdCarron@users.noreply.github.com>
2020-08-23 19:46:34 -0300

    Completa 14. PROYECTO Generador de Nombres con REST API y AJAX

--- js/app.js
+++ js/app.js
@@ -1,1 +1,44 @@
-console.log("Hola Mundo!");
+const $formulario = document.querySelector("#generar-nombre");
+$formulario.addEventListener("submit", generarNomrbes);
+
+function generarNomrbes(e) {
+  e.preventDefault();
+  const $origen = document.querySelector("#origen");
+  const origenSeleccionado = $origen.options[$origen.selectedIndex].value;
+  const $genero = document.querySelector("#genero");
+  const generoSeleccionado = $genero.options[$genero.selectedIndex].value;
+  const $cantidad = document.querySelector("#numero");
+  const cantidadSeleccionado = $cantidad.value;
+
+  let apiUrl = "";
+  apiUrl += "https://randomuser.me/api/?";
+
+  if (origenSeleccionado !== "") {
+    apiUrl += `nat=${origenSeleccionado}&`;
+  }
+  if (generoSeleccionado !== "") {
+    apiUrl += `gender=${generoSeleccionado}&`;
+  }
+  if (cantidadSeleccionado !== "") {
+    apiUrl += `results=${cantidadSeleccionado}&`;
+  }
+
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", apiUrl, true);
+  xhr.onload = function () {
+    if (this.status === 200) {
+      const xhrResponse = JSON.parse(this.responseText);
+      const listaNombres = xhrResponse.results;
+
+      const $divResultado = document.querySelector("div#resultado");
+      let htmlNombres = `<h3>Nombres Generados</h3>`;
+      htmlNombres += `<ul class="lista">`;
+      listaNombres.forEach((nombre) => {
+        htmlNombres += `<li>${nombre.name.first}</li>`;
+      });
+      htmlNombres += `</ul>`;
+      $divResultado.innerHTML = htmlNombres;
+    }
+  };
+  xhr.send();
+}
